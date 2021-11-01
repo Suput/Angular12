a99e68b4f4a5f79d479e27f784bed939515f6229
Nick <40500378+Suput@users.noreply.github.com>
2021-11-01 17:44:24 +0300

    Initial commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,27 @@
+let num1 = 10;
+let num2 = 11;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+console.log(19 % 4);
+
+let str1 = "string";
+let str2 = "20";
+console.log(str1 + str2);
+
+let bool1 = true;
+console.log(bool1);
+console.log(!bool1);
+console.log(!!bool1);
+
+let obj1 = {
+    name: "Ivan",
+    surname: "Petrov",
+    middlename: "Aleksandrovich",
+    age: 42,
+    isMale: true
+}
+
+console.log(obj1);
